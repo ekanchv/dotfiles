b85da6ac9b94e6f8e45eb51151ad22f625c60237
esh-k <esh.eshwar.eshwar.esh@gmail.com>
2026-07-05 00:17:32 -0700

    fixed bufferline deletion

--- private_dot_config/nvim/lua/plugins/bufferline.lua
+++ private_dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,30 +1,30 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
-    keys = {
-      { "]b", "<cmd>BufferLineCycleNext<CR>", desc = "next buffer/tab" },
-      { "[b", "<cmd>BufferLineCyclePrev<CR>", desc = "prev buffer/tab" },
-      { "<leader>x", "<cmd>bdelete<CR>", desc = "close buffer" },
-    },
-    opts = function()
-      return {
-        highlights = require("catppuccin.special.bufferline").get_theme(),
-        options = {
-          mode = "buffers",
-          diagnostics = "nvim_lsp",
-          always_show_bufferline = true,
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              highlight = "Directory",
-              separator = true,
-            },
-          },
-        },
-      }
-    end,
-  },
+	{
+		"akinsho/bufferline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
+		keys = {
+			{ "]b", "<cmd>BufferLineCycleNext<CR>", desc = "next buffer/tab" },
+			{ "[b", "<cmd>BufferLineCyclePrev<CR>", desc = "prev buffer/tab" },
+			{ "<leader>x", "<cmd>BufferLineCyclePrev<CR><cmd>BufferLineCloseRight<CR>", desc = "close buffer" },
+		},
+		opts = function()
+			return {
+				highlights = require("catppuccin.special.bufferline").get_theme(),
+				options = {
+					mode = "buffers",
+					diagnostics = "nvim_lsp",
+					always_show_bufferline = true,
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							highlight = "Directory",
+							separator = true,
+						},
+					},
+				},
+			}
+		end,
+	},
 }
